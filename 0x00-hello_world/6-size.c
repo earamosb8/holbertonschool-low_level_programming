ee7e1511f2a14df1fcf7851404789276a0ba582a
somarae8 <1493@holbertonschool.com>
2020-02-05 21:16:49 +0000

    sd

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("Size of a char:", sizeof(char);
+printf("Size of a  int:", sizeof(int);
+printf("Size of a long int:", sizeof(long long int);
+Size of a long long int: 8 byte(s);
+Size of a float: 4 byte(s);
+return (0);
+}
+
+
