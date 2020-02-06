596970a3f1732f76e564213acded9ac475d16144
somarae8 <1493@holbertonschool.com>
2020-02-06 19:41:13 +0000

    we

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a, b;
+
+	for (a = 97; a < 123; a++)
+	{
+		putchar(a);
+	}
+	for (b = 65; b < 91; b++)
+	{
+		putchar(b);
+	}
+		putchar('\n');
+	return (0);
+}
