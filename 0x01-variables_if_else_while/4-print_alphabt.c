065b0daf32f75041691bd1af2cc3f978ecf129d9
somarae8 <1493@holbertonschool.com>
2020-02-06 20:06:01 +0000

    w

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 97; a < 101; a++)
+	{
+		putchar(a);
+	}
+	for (a = 102; a < 113; a++)
+	{
+		putchar(a);
+	}
+	for (a = 114; a < 123; a++)
+	{
+		putchar(a);
+	}
+		putchar('\n');
+	return (0);
+}
