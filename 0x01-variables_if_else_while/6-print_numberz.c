6c67763c2424a4c96b59ee12c7e788fb38dbb69e
somarae8 <1493@holbertonschool.com>
2020-02-06 20:52:00 +0000

    ww

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 48; a < 58; a++)
+	{
+	putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
