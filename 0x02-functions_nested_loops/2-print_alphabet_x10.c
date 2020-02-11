f518324877dcd1f4783a36355206bf332a29b759
somarae8 <1493@holbertonschool.com>
2020-02-11 13:57:15 +0000

    sd

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,8 +1,7 @@
 #include "holberton.h"
 
-/*
- * print_alphabet_x10 - writes the alphabet x10
- *
+/**
+ * print_alphabet_x10 - writes the character c to stdout
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
