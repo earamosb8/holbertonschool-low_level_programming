3c0d28e1cd29d620e4d2df509ace8dbd83e3e66d
somarae8 <1493@holbertonschool.com>
2020-02-13 02:02:06 +0000

    ww

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -11,15 +11,15 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && b < c)
+	if (a > b && b <= c)
 	{
 		largest = a;
 	}
-	else if (b > a && b > c)
+	else if (b > c && c >= a)
 	{
 		largest = b;
 	}
-	else if (c > a && a < b)
+	else
 	{
 		largest = c;
 	}
