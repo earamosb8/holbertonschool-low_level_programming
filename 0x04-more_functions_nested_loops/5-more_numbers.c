a50fee2d9b31825f129c49a794e00124b4f35dad
somarae8 <1493@holbertonschool.com>
2020-02-13 17:36:09 +0000

    ss

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+ * more_numbers - writes the character c to stdout
+ *
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void more_numbers(void)
+{
+int a, b, d, i;
+
+	for (a = 0; a <= 10; a++)
+	{
+		for (b = 0; b <= 14; b++)
+		{
+			if (b > 9)
+			{
+				d = b / 10;
+				_putchar(d + '0');
+			}
+			i = b % 10;
+			_putchar(i + '0');
+		}
+		_putchar('\n');
+	}
+}
