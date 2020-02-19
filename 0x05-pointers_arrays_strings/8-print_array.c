ad569ee28b4800bea7670f24eba67a6943d8a51e
somarae8 <1493@holbertonschool.com>
2020-02-19 07:19:19 +0000

    ww

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_array - function that prints a string
+ * @a: ddddddd
+ * @n: n numbers
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i == n - 1)
+			printf("%d", a[i]);
+		else
+			printf("%d, ", a[i]);
+	}
+	printf("\n");
+}
