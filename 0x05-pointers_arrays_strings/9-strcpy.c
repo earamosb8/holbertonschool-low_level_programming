49c4e26b652c5ef11e48e9a0249756ef304611c5
somarae8 <1493@holbertonschool.com>
2020-02-19 20:22:32 +0000

    ww

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,29 +1,22 @@
 #include "holberton.h"
 #include <stdio.h>
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
+ * _strcpy - writes the character c to stdout
+ * @dest: The character to print
+ * @src: kkdfkdkfdk
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
 char *_strcpy(char *dest, char *src)
 {
 
-	int a=0, i=0;
+	int a = 0;
 
 	while (src[a] != '\0')
 	{
+		dest[a] = src[a];
 		a++;
 	}
-
-	a--;
-
-	while( i <= a)
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	return (dest);
+	return (dest + '\0');
 }
 
