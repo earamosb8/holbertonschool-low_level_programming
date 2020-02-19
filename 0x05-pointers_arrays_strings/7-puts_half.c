70fb61087e264032ca7f462b5282dd9ec6185ce4
somarae8 <1493@holbertonschool.com>
2020-02-19 05:23:12 +0000

    ss

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+/**
+ * puts_half - writes the character c to stdout
+ * @str: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void puts_half(char *str)
+{
+
+	int a = 0, c, n;
+
+	while (str[a] != '\0')
+	{
+		a++;
+
+	}
+	c = a % 2;
+	if (c == 0)
+	{
+		n = a / 2;
+	}
+	else
+	{
+		n = (a - 1) / 2;
+	}
+	while (str[n] != '\0')
+	{
+		_putchar(str[n]);
+		n++;
+
+	}
+	_putchar('\n');
+}
+
