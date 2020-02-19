828ed8b3a64ecb8688e7e1a43970b88429d429ba
somarae8 <1493@holbertonschool.com>
2020-02-19 03:49:30 +0000

    ww

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,21 +1,30 @@
+#include "holberton.h"
 #include <stdio.h>
 #include <string.h>
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
+ * puts2 - writes the character c to stdout
+ * @str:---------------
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
 void puts2(char *str)
 {
-int a=0, b;
-a = strlen(str);
-printf("%d",a);
-for (b = 0; b >=a; b++)
-{
-	str[b];
-printf("%c",str);
-}
-printf("\n");
+
+int a = 0, c;
+int b = strlen(str);
+
+	while (a <= b)
+	{
+	c = a % 2;
+	if (c == 0)
+	{
+		printf("%c", str[a]);
+	}
+
+		a++;
+	}
+
+		printf("\n");
+
 }
