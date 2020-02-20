d985c89a805485d55a53f747a7a2b3725ab70742
somarae8 <1493@holbertonschool.com>
2020-02-20 16:10:52 +0000

    ww

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,32 @@
+/**
+ * _strncat - writes the character c to stdout
+ * @dest: The character to print
+ * @src: asdasd
+ * @n: dsdfss
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int a = 0, b = 0, c = 0;
+
+	while (dest[a] != '\0')
+	{
+		a++;
+	}
+	while (src[b] != '\0')
+	{
+		b++;
+	}
+	if (n > b)
+	{
+		n = b;
+	}
+	while (c < n)
+	{
+		dest[a] = src[c];
+		c++;
+		a++;
+	}
+	return (dest);
+}
