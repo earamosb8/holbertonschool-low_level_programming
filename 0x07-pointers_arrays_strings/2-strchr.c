491a81d7dbf13fc44554aef1bcc032f4b4bf593a
somarae8 <1493@holbertonschool.com>
2020-02-24 20:36:08 +0000

    www

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -16,12 +16,13 @@ char *_strchr(char *s, char c)
 		{
 			return (s + i);
 		}
-		else if (s[i] == '\0')
-		{
-			return (NULL);
-		}
+
 		i++;
 	}
+if (s[i] == '\0')
+{
+
 return (NULL);
 }
-
+return (NULL);
+}
