598024d7dfa19eb291864ec03acb65904b065210
somarae8 <1493@holbertonschool.com>
2020-02-24 15:56:50 +0000

    dd

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * _memset - writes the character c to stdout
+ * @s: The character to print
+ * @b: dsfsdf
+ * @n:sdfsdfsf
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i, c = n;
+
+	for (i = 0; i < c; i++)
+	{
+		s[i] = b;
+
+	}
+	return (s);
+
+}
