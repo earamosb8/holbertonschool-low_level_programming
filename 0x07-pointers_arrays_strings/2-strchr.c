5e2d1f6fa6b5564c7e7ede314afe4dd44d068bb9
somarae8 <1493@holbertonschool.com>
2020-02-24 20:15:36 +0000

    www

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,10 +10,15 @@ char *_strchr(char *s, char c)
 {
 	int i = 0;
 
-	while (s[i] != c)
+	while (s[i] != '\0')
 	{
-		i++;
+		if (s[i] == c)
+		{
+			return (s + i);
+		}
+
+	i++;
 	}
-return (s + i);
+return (NULL);
 }
 
