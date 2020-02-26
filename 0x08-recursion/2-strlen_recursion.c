e899570e97574d07525db02f030267ecd5a630f7
somarae8 <1493@holbertonschool.com>
2020-02-26 13:09:19 +0000

    ww

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+/**
+ * _strlen_recursion -  lengt string
+ * @s: variable
+ *
+ *
+ * .
+ */
+int _strlen_recursion(char *s)
+{
+	int n=0;
+
+	if (*s != '\0')
+	{
+	n = _strlen_recursion(s + 1);
+	n++;
+	}
+return (n);
+}
