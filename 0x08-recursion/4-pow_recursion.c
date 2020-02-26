b06ea4856fe30ddd93f5a879de0dd017c651f8dc
somarae8 <1493@holbertonschool.com>
2020-02-26 17:06:41 +0000

    ww

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _pow_recursion(int x, int y)
+{
+int r;
+if(y < 0)
+{
+return -1;
+}
+if (y > 0)
+return r = x * ;
+}
