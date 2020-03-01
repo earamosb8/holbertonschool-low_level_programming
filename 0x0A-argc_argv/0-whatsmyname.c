1e73af835c987b19ae7bfc626f91f291b7bbbff0
somarae8 <1493@holbertonschool.com>
2020-03-01 03:24:31 +0000

    ww

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - arcg, *argv print the name of program
+ * @argc: count parameters
+ * @argv: array parameters
+ * Return: return
+ *
+ *
+ */
+int main(int argc, char *argv[])
+
+	argc = argc;
+	printf("%s\n", argv[0]);
+return (0);
+}
+
+
