d13df17bb1ecfa888e5273dae55d4951def13574
somarae8 <1493@holbertonschool.com>
2020-03-04 18:30:36 +0000

    w

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,20 +11,26 @@ char *str_concat(char *s1, char *s2)
 	char *newstring;
 
 	if (s1 == NULL)
+	{
 		s1 = malloc(1);
+		*s1 = '\0';
+	}
 	if (s2 == NULL)
+	{
 		s2 = malloc(1);
+		*s2 = '\0';
+	}
 	if (s1 == NULL && s2 == NULL)
 	{
 		s1 = malloc(1);
 		s2 = malloc(1);
+		*s2 = '\0';
+		*s1 = '\0';
 	}
-	while (s1[size1] != '\0')
+	while(s1[size1] != '\0')
 		size1++;
 	while (s2[size2] != '\0')
-	{
 		size2++;
-	}
 	totalsize = size1 + size2;
 	newstring = malloc(sizeof(char) * totalsize);
 	if (newstring == NULL)
@@ -34,17 +40,16 @@ char *str_concat(char *s1, char *s2)
 		if (a < size1)
 		{
 			newstring[contador] = s1[a];
-			contador++;
 			a++;
+			contador++;
 		}
 		if (a == size1)
 		{
 			newstring[contador] = s2[b];
-			contador++;
 			b++;
+			contador++;
 		}
 	}
-
 	return (newstring);
 }
 
