5b49878d5a16814978fd1bbbfdb3d7fe26fd5cb5
somarae8 <1493@holbertonschool.com>
2020-03-04 02:45:20 +0000

    ss

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,7 +1,13 @@
 #include <stdlib.h>
+/**
+ * str_concat---concatenates two strings
+ * @s1: string 1
+ * @s2: string 2
+ * Return: string
+ */
 char *str_concat(char *s1, char *s2)
 {
-	int size1=0, size2=0, totalsize, contador = 0,a=0,b=0;
+	int size1 = 0, size2 = 0, totalsize, contador = 0, a = 0, b = 0;
 	char *newstring;
 
 	if (s1 == NULL || s2 == NULL)
@@ -12,32 +18,30 @@ char *str_concat(char *s1, char *s2)
 	{
 		size1++;
 	}
-
- 	while (s2[size2] != '\0')
-        {
-                size2++;
-        }
-
-	totalsize = size1+size2;
+	while (s2[size2] != '\0')
+	{
+		size2++;
+	}
+	totalsize = size1 + size2;
 	newstring = malloc(sizeof(char) * totalsize + 1);
 	if (newstring == NULL)
 	{
-		return(NULL);
+		return (NULL);
 	}
 	while (contador <= totalsize)
 	{
 
-		if(a < size1)
+		if (a < size1)
 		{
-		newstring[contador] = s1[a];
-		contador++;
-		a++;
+			newstring[contador] = s1[a];
+			contador++;
+			a++;
 		}
-		if(a == size1)
+		if (a == size1)
 		{
-		newstring[contador] = s2[b];
-		contador++;
-		b++;
+			newstring[contador] = s2[b];
+			contador++;
+			b++;
 		}
 	}
 
