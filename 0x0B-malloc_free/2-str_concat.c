0d83a27b391722b41cba43ab63d8c6d5a3b835b0
somarae8 <1493@holbertonschool.com>
2020-03-04 19:50:27 +0000

    q

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,7 +12,10 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1 == NULL && s2 == NULL)
 	{
-		s1 = malloc(1), s2 = malloc(1), *s1 = '\0', *s2 = '\0';
+		s1 = malloc(1);
+		s2 = malloc(1);
+		*s1 = '\0';
+		*s2 = '\0';
 	}
 	else if (s1 == NULL && s2 != NULL)
 	{
