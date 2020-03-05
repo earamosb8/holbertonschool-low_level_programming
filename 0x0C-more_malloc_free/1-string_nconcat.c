7fa96963dad6df62b38a214da113056a855b00ba
somarae8 <1493@holbertonschool.com>
2020-03-05 19:43:19 +0000

    q

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,33 +1,33 @@
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+/**
+ * string_nconcat---concatenates two strings
+ * @s1: string 1
+ * @s2: string 2
+ * @n: integer
+ * Return: string
+ */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *p;
-	unsigned int size1,size2, sizet, c = 0,b = 0;
+	unsigned int size1, size2, sizet, c = 0, b = 0;
 	unsigned int d = 0;
+
 	if (s1 == NULL && s2 == NULL)
 	{
 		s1 = malloc(1), s2 = malloc(1), *s1 = '\0', *s2 = '\0';
 	}
 	else if (s1 == NULL && s2 != NULL)
 	{
-		s1 = malloc(1);
-		*s1 = '\0';
+		s1 = malloc(1), *s1 = '\0';
 	}
 	else if (s2 == NULL && s1 != NULL)
 	{
 		s2 = malloc(1), *s2 = '\0';
 	}
-
-	while(s1[size1] != '\0')
-	{
+	while (s1[size1] != '\0')
 		size1++;
-	}
- 	while(s2[size2] != '\0')
-        {
-                size2++;
-        }
+	while (s2[size2] != '\0')
+		size2++;
 	if (n >= size2)
 	{
 		n = size2;
@@ -36,32 +36,16 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		n = n;
 	}
-	sizet = size1 + n;
-	p = malloc(sizeof(char) * sizet);
+	sizet = size1 + n, p = malloc(sizeof(char) * sizet);
 	if (p == NULL)
-	{
 		return (NULL);
-	}
-	while(b <= sizet)
+	while (b <= sizet)
 	{
 		if (b < size1)
-		{
-			p[b] = s1[c];
-			b++;
-			c++;
-		}
+			p[b] = s1[c], b++, c++;
 		if (b >= size1)
-		{
-			p[b] = s2[d];
-			b++;
-			d++;
-		}
+			p[b] = s2[d], b++, d++;
 	}
-if(p == NULL)
-{
-return (NULL);
-}
-	p[b] = '\0';
-	return(p);
+	return (p);
 }
 
