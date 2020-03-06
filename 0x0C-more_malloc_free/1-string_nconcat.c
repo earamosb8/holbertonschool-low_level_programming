3a88f384e11f2da35c6ec858415ba88a72a37542
somarae8 <1493@holbertonschool.com>
2020-03-06 04:05:50 +0000

    d

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,46 +1,40 @@
 #include <stdlib.h>
 /**
- * string_nconcat---concatenates two strings
- * @s1: string 1
- * @s2: string 2
- * @n: integer
- * Return: string
+ * *string_nconcat - create array
+ * @s1: input array
+ * @s2: input array
+ * @n: number of chat to concatenate
+ * Return: null in error
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *p;
-	unsigned int size1, size2, sizet, c = 0, b = 0;
-	unsigned int d = 0;
+	unsigned int i = 0, size1 = 0, size2 = 0, j = 0;
+	char *str_;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	while (s1[size1] != '\0')
+	while (*(s1 + size1))
 		size1++;
-	while (s2[size2] != '\0')
+	while (*(s2 + size2))
 		size2++;
 	if (n < size2)
+		size2 = n;
+	str_ = malloc((sizeof(char) * size1 + sizeof(char) * size2) + 1);
+	if (str_ != NULL)
 	{
-	sizet = size1 + n, p = malloc(sizeof(char) * sizet + 1);
-	}
-
-	if (p != NULL)
-	{
-		while (b <= sizet)
+		for (; i < size1; i++)
+			*(str_ + i) = *(s1 + i);
+		for (; j < size2; j++)
 		{
-			if (b < size1)
-				p[b] = s1[c], b++, c++;
-			if (b >= size1)
-				p[b] = s2[d], b++, d++;
+			*(str_ + i) = *(s2 + j);
+			i++;
 		}
 	}
 	else
-	{
 		return (NULL);
-	}
-
-	p[b] = '\0';
-	return (p);
+	str_[i] = '\0';
+	return (str_);
 }
 
