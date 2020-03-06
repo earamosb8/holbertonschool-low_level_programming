ebc93caee3e76ae7dd9d70256f6c9c2a56bcd4f8
somarae8 <1493@holbertonschool.com>
2020-03-06 01:29:31 +0000

    w

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,13 +13,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int d = 0;
 
 	if (s1 == NULL)
-	{
-		s1 = " ";
-	}
+		s1 = "";
 	if (s2 == NULL)
-	{
-		s2 = " ";
-	}
+		s2 = "";
 	while (s1[size1] != '\0')
 		size1++;
 	while (s2[size2] != '\0')
