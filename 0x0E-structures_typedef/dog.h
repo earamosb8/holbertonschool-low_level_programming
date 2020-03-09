5de602f73815d3364edaded3b9dd6c3cc1d5d1cf
somarae8 <1493@holbertonschool.com>
2020-03-09 21:28:16 +0000

    fix

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - struct of dog.
+ * @name: dogs name
+ * @owner: ownwers dog
+ * @age: age of dog
+ */
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+#endif
+
