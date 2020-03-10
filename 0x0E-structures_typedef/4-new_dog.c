911bf7243748f42d1f495927a0feab0ce12947d7
somarae8 <1493@holbertonschool.com>
2020-03-10 04:55:27 +0000

    s

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,82 +1,47 @@
-#include <stdlib.h>
 #include "dog.h"
+#include <stdlib.h>
 /**
- * _strcpy - writes the character c to stdout
- * @dest: The character to print
- * @src: kkdfkdkfdk
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * new_dog - initialize a variable of type struct dog.
+ * @name: name parameter
+ * @age: age parameter
+ * @owner: owner parameter
+ * Return: pointer
  */
-char *_strcpy(char *dest, char *src)
-{
 
-	int a = 0;
-
-	while (src[a] != '\0')
-	{
-		dest[a] = src[a];
-		a++;
-	}
-	dest[a] = '\0';
-	return (dest);
-}
-/**
- * _strlen ---acter c to stdout
- * @s: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _strlen(char *s)
-{
-int a;
-
-	while (s[a] != '\0')
-	{
-		a++;
-	}
-	return (a);
-}
-/**
- * dog_t *new_dog ---new_dog
- * @name: name to copy
- * @age: age of dog
- * @owner: th owner
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	int lename, lenowner;
-	char *name2, *owner2;
+	dog_t *new;
+	char *newname, *newowner;
+	int i = 0, j = 0, k;
 
-	dog_t *miperro;
+	new = malloc(sizeof(dog_t));
+	if (new == NULL)
+		return (NULL);
 
-	lename = _strlen(name);
-	lenowner = _strlen(owner);
-	miperro = malloc(sizeof(dog_t));
-	if (miperro == NULL)
+	while (name[i])
+		i++;
+	newname = malloc(i + 1);
+	if (newname == NULL)
 	{
+		free(new);
 		return (NULL);
 	}
-	name2 = malloc(lename + 1);
-	if (name2 == NULL)
+	while (owner[j])
+		j++;
+	newowner = malloc(j + 1);
+	if (newowner == NULL)
 	{
-		free(miperro);
+		free(newname);
+		free(new);
 		return (NULL);
 	}
-	owner2 = malloc(lenowner + 1);
-	if (miperro == NULL)
-	{
-		free(name2);
-		free(miperro);
-		return (NULL);
-	}
-	_strcpy(name2, name);
-	_strcpy(owner2, owner);
-	miperro->name = name2;
-	miperro->age = age;
-	miperro->owner = owner2;
-	return (miperro);
-
+	for (k = 0; k <= i; k++)
+		newname[k] = name[k];
+	for (k = 0; k <= j; k++)
+		newowner[k] = owner[k];
+	new->name = newname;
+	new->age = age;
+	new->owner = newowner;
+	return (new);
 }
+
