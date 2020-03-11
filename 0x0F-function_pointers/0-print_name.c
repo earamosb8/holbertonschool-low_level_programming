726616ca2c4646240febc67ee5dca264354471c0
somarae8 <1493@holbertonschool.com>
2020-03-11 17:11:45 +0000

    2

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,7 +1,12 @@
+/**
+ * print_name - check the code for Holberton School students.
+ * @name: char to print
+ * @f: pointer to function
+ */
 #include <stdlib.h>
-void print_name(char *name, void f(char *))
+void print_name(char *name, void (*f)(char *))
 {
-	if(*f != NULL)
+	if (*f != NULL)
 	{
 		f(name);
 	}
