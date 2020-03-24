d8361919968c43830a38c6d511c88b0839081b91
somarae8 <1493@holbertonschool.com>
2020-03-24 18:19:34 +0000

    insert node

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -24,7 +24,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		if (tmp != NULL)
 		{
 
-			tmp = (tmp)->next;
+			tmp = tmp->next;
 		}
 		else
 		{
@@ -39,8 +39,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	if (idx == 0)
 	{
-	nuevo->next = *head;
-	*head = nuevo;
+		nuevo->next = *head;
+		*head = nuevo;
 	}
 	else if (tmp != NULL)
 	{
