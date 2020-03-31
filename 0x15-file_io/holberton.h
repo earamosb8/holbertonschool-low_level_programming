4f2618e795a796540bebc96faf03210283644a82
somarae8 <1493@holbertonschool.com>
2020-03-31 21:59:49 +0000

    first

--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -1,6 +1,6 @@
-#include<stdio.h>
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
+#include<stdio.h>
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
