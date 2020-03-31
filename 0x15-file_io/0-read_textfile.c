6a33b597ad3ae06edf833149aa9c826642d61fb3
somarae8 <1493@holbertonschool.com>
2020-03-31 21:57:26 +0000

    first

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,43 @@
+#include "holberton.h"
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+/**
+ * read_textfile - reads a textfile and prints it to stdout
+ *
+ * @filename: name of file to print
+ * @letters: letters to print
+ *
+ * Return: Null on failure
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+
+	int fd;
+	int r;
+	char *text;
+
+	if (filename == NULL)
+	{
+		return (0);
+	}
+	fd = open(filename,  O_RDONLY);
+	if (fd == -1)
+	{
+		return (0);
+	}
+	text = malloc(sizeof(char) * letters);
+	if (text == NULL)
+	{
+		return (-1);
+	}
+
+	r = read(fd, text, letters);
+	if (r == -1)
+	{
+		return (0);
+	}
+
+	write(STDOUT_FILENO, text, r);
+	return (letters);
+}
--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -1,6 +1,7 @@
+#include<stdio.h>
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
-size_t read_textfile(const char *filename, size_t letters);
+ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 int _putchar(char c);
