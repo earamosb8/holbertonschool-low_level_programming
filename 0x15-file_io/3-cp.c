e31ae1e2754d119dd79147834ad6e4594a10c2ae
somarae8 <1493@holbertonschool.com>
2020-04-01 04:18:48 +0000

    cp

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,46 @@
+#include "holberton.h"
+/**
+ * main - copies a file
+ *
+ * @argc: argument cont
+ * @argv: arguments
+ * Return: Returns 97 for syntax error, 98 for read error, 99 write, 100 close
+ */
+int main(int argc, char *argv[])
+{
+	int opf = 0;
+	int opc = 0;
+	int rd = MAX_SIZE, wr, c1, c2;
+	char temp[MAX_SIZE];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+	}
+	opf = open(argv[1], O_RDONLY);
+	if (opf == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
+	}
+	opc = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
+	if (opc == -1)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+
+	while (rd == MAX_SIZE)
+	{
+		rd = read(opf, temp, MAX_SIZE);
+		wr = write(opc, temp, rd);
+	}
+	c1 = close(opf);
+	c2 = close(opc);
+	if (c1 == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", opf), exit(100);
+	if (c2 == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", opc), exit(100);
+	if (rd == -1)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
+	if (wr <= -1)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+
+	return (0);
+}
