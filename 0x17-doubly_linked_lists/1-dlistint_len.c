9c08e42239f276d9c5a59483d9ad9f01c372a4e1
somarae8 <1493@holbertonschool.com>
2020-05-01 17:46:42 +0000

    1-dlistint_len.c

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * dlistint_len - return length of list
+ * @h: pointer to list
+ * Return: size of list
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t n;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		n++;
+	}
+	return (n);
+}
