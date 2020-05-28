f75eba3fe07a125b5ad605bce6f6219125d3339a
somarae8 <1493@holbertonschool.com>
2020-05-28 21:45:51 +0000

    first commit

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create - create a hash table with chaining
+ * @size: size of hash table
+ *
+ * Return: pointer to hash_table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *h;
+	if (size == 0)
+        {
+                return(NULL);
+        }
+	h = malloc(sizeof(hash_table_t) * 1);
+	if (h == NULL)
+	{
+		return(NULL);
+	}
+	h->array = malloc(sizeof(hash_node_t *) * size);
+	if (h->array == NULL)
+	{
+		free (h);
+		return NULL;
+	}
+
+	return (h);
+}
